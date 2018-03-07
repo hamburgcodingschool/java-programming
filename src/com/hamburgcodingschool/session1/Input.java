e0026699ff43d092c970b65e9eba4e51489a89f2
Teresa Holfeld <teresa.holfeld@gmail.com>
2018-03-07 12:12:12 +0100

    Move content to package session1

--- src/com/hamburgcodingschool/session1/Input.java
+++ src/com/hamburgcodingschool/session1/Input.java
@@ -1,4 +1,4 @@
-package com.hamburgcodingschool;
+package com.hamburgcodingschool.session1;
 
 import java.util.Scanner;
 
--- src/com/hamburgcodingschool/session1/Loops.java
+++ src/com/hamburgcodingschool/session1/Loops.java
@@ -1,4 +1,4 @@
-package com.hamburgcodingschool;
+package com.hamburgcodingschool.session1;
 
 public class Loops {
 
